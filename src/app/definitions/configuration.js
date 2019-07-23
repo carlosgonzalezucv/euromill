00bad226f5b3e2cc399b97323be1673070a76e4e
CG <carlos.gonzalez@sitiouno.com>
2019-07-23 08:05:56 -0400

    avances

--- src/app/definitions/configuration.js
+++ src/app/definitions/configuration.js
@@ -7,7 +7,7 @@
     let env = window.location.hostname;
     return {
       url: {
-        "euromill.herokuapp": "https://ancient-river-66765.herokuapp.com",
+        "euromill.herokuapp.com": "https://ancient-river-66765.herokuapp.com",
         localhost: "http://localhost:8080"
       }[env]
     }
