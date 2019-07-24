610bf8b78891ce762c7c9d7134e512cde4d2c0c5
CG <carlos.gonzalez@sitiouno.com>
2019-07-23 22:24:49 -0400

    hotfix

--- src/app/directives/result-view/result-view.directive.js
+++ src/app/directives/result-view/result-view.directive.js
@@ -26,7 +26,6 @@
 		return {
 			restrict: 'E',
       template: `
-        <h4 class="m-l-xs" ng-if="index == 0"> {{title}} </h4>
         <div class="custom-bg p-xs m-xs" >
           <div class="font-italic" style="display: flex; justify-content: space-between">
             <span class="p-xs text-center" ng-repeat="num in array track by $index"> {{num | digits}} </span>
--- src/app/main/groups.controller.js
+++ src/app/main/groups.controller.js
@@ -4,6 +4,10 @@
   angular
     .module('inspinia')
     .controller('GroupsController', GroupsController);
+    
+  const SortNumbers = asc => (a,b) => asc * (a-b) / Math.abs(a-b);
+  const MATRIX_SIZE = 4;
+  const UNIT_MEASURE = 14;
 
   GroupsController.$inject = ['MainService', 'LEGService'];
   function GroupsController(MainService, LEGService) {
@@ -20,10 +24,23 @@
           while (row.length < 5) {
             row.push( -1 );
           }
+          row = row.sort(SortNumbers(1));
         });
+
+        doc.coordinates = doc.positions.map(row => row.toXY());
 
         doc.flatNumeros = doc.numeritos.map(numeritos => numeritos.flat());
       });
     }
+  }
+  Array.prototype.toXY = function () {
+    let self = this;
+
+    return self.filter(e => e > -1).map(position => {
+      let offsetY = Math.floor(position / MATRIX_SIZE);
+      let offsetX = position % MATRIX_SIZE;
+
+      return [offsetX * UNIT_MEASURE, offsetY * UNIT_MEASURE].map(e => e + "px");
+    });
   }
 })();
