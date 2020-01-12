3538f31ff68a9a730bbe31ca8f9bd8ba295e3e4c
Carlos <carlos.gonzalez@sitiouno.com>
2020-01-12 11:50:48 -0400

    avances

--- src/app/main/main.Controller.js
+++ src/app/main/main.Controller.js
@@ -6,7 +6,7 @@
     MainController.$inject = ["MainService", "$scope"];
     function MainController(MainService, $scope) {
 
-      var vm = this, {getGlobalStats, getResults} = MainService;      
+      var vm = this, {getGlobalStats, getResults} = MainService;
       vm.onLoadData = onLoadData;
       vm.loading = false;
       vm.updateResults = updateResults;
@@ -18,7 +18,7 @@
       vm.showColumn = showColumn;
       vm.updateMod = updateMod;
       vm.selectedAxes = '0,1,2';
-      
+
       start();
 
       function start() {
@@ -29,6 +29,7 @@
       function _updateAll () {
         vm.results2 = getResults().reverse();
         vm.results = vm.results2.map(({results, date, stars}) => ({results: [...results, ...stars], date: new Date(date)}))
+        vm.controlDay = computeAccumDays(vm.results2.map(({results}) => results));
         vm.stats.global = getGlobalStats(vm);
         vm.onlyResults = vm.results2.map(e => [...e.results, ...e.stars]);
         vm.dates = vm.results2.map(e => e.date);
@@ -48,6 +49,16 @@
           .then(r => (vm.loading = false))
           .then(() => start())
           .catch(r => (vm.loading = false));
+      }
+      function computeAccumDays(results) {
+        let computedDays = new Array(50).fill(0);
+        let aux = [];
+        for(let currentIndex = 0; currentIndex < results.length; currentIndex++) {
+          computedDays.forEach((elem, i) => computedDays[i]++);
+          results[currentIndex].forEach((e,i) => computedDays[e - 1] = 0);
+          aux.push(angular.copy(computedDays));
+        }
+        console.log("aux", aux);
       }
     }
 })();
