85dde9836af84a33ae0356f3b4c074baf498dc2f
Carlos <carlos.gonzalez@sitiouno.com>
2020-01-22 17:14:31 -0400

    se incluye filtro para reseteo de contador de dias

--- src/app/main/main.Controller.js
+++ src/app/main/main.Controller.js
@@ -15,8 +15,28 @@
       };
       vm.globalModule = 5;
       vm.column = 1;
+      vm.cutIndex = { value: null, label: "Ninguno" };
+      vm.cutIndexOptions = [
+        {
+          value: null,
+          label: "Ninguno"
+        },
+        {
+          value: 3,
+          label: 3
+        },
+        {
+          value: 7,
+          label: 7
+        },
+        {
+          value: 15,
+          label: 15
+        }
+      ];
       vm.showColumn = showColumn;
       vm.updateMod = updateMod;
+      vm.updateControlDay = updateControlDay;
       vm.selectedAxes = '0,1,2';
       vm.formatter = digit => {
         return digit.toString().length === 1 ? `0${ digit }` : digit.toString();
@@ -31,8 +51,8 @@
       function _updateAll () {
         vm.__results = getResults();
         vm.results2 = vm.__results.reverse();
-        vm.results = vm.results2.map(({results, date, stars}) => ({results: [...results, ...stars], date: new Date(date)}))
-        vm.controlDay = computeAccumDays(vm.__results.map(({results}) => results));
+        vm.results = vm.results2.map(({results, date, stars}) => ({results: [...results, ...stars], date: new Date(date)}));
+        updateControlDay();
         vm.stats.global = getGlobalStats(vm);
         vm.onlyResults = vm.results2.map(e => [...e.results, ...e.stars]);
         vm.dates = vm.results2.map(e => e.date);
@@ -53,15 +73,33 @@
           .then(() => start())
           .catch(r => (vm.loading = false));
       }
-      function computeAccumDays(results) {
+      function computeAccumDays(results, resetIndex = null) {
         let computedDays = new Array(50).fill(0);
         let aux = [];
         for(let currentIndex = 0; currentIndex < results.length; currentIndex++) {
-          computedDays.forEach((elem, i) => computedDays[i]++);
+          // console.log("resetIndex vs currentIndex", resetIndex, currentIndex);
+          computedDays.forEach((elem, i) =>{ 
+            if(resetIndex && currentIndex % resetIndex === 0) {
+              computedDays[i] = 1;              
+            } else { 
+              computedDays[i]++;
+            }
+          });
           results[currentIndex].forEach((e,i) => computedDays[e - 1] = 0);
           aux.push(angular.copy(computedDays.map(vm.formatter)));
         }
         return aux;
+      }
+      function updateControlDay(index = null){
+        vm.loading = true;
+        setTimeout(
+          () => {
+            vm.controlDay = computeAccumDays(vm.__results.map(({results}) => results), index);
+            vm.loading = false;
+            $scope.$apply();
+          },
+          500          
+        );
       }
     }
 })();
