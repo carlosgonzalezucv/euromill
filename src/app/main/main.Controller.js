500661d485997c26c4d44c405c9d850e91f348aa
Carlos <carlos.gonzalez@sitiouno.com>
2020-01-22 17:21:05 -0400

    rangos extremos

--- src/app/main/main.Controller.js
+++ src/app/main/main.Controller.js
@@ -22,8 +22,20 @@
           label: "Ninguno"
         },
         {
+          value: 1,
+          label: 1
+        },
+        {
+          value: 2,
+          label: 2
+        },
+        {
           value: 3,
           label: 3
+        },
+        {
+          value: 5,
+          label: 5
         },
         {
           value: 7,
